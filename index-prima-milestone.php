c5cfef47b92108a0150c2672c35709b6ecf1b441
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-08-30 16:38:32 +0200

    prima bozza milestone 1

--- index-prima-milestone.php
+++ index-prima-milestone.php
@@ -1,13 +1,70 @@
+<?php
+
+    require_once __DIR__ . '/backend/database.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Document</title>
+    <title>Dischi</title>
 </head>
 <body>
+    <header>
+        <div class="logo-container">
+            <img src="./img/spotify-logo.png" alt="logo Spotify">
+        </div>
+
+        <div class="select-container">
+            <label for="genre">Scegli la tua musica</label>
+            <!-- <select v-model="selectGenre" @change="$emit('choice', selectGenre)" id="genre">
+                <option v-for="genre in optionGenre" :key="genre.id" 
+                    :value="genre.value">
+                    {{ genre.text }}
+                </option>
+            </select> -->
+
+            <input type="text">
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row-me">
+
+                <?php 
+                    foreach ($albums as $album) { 
+                ?>
+                <div class="album-container">
+                    <div class="album-inner">
+                        <div class="img-container">
+                            <img src="<?php echo $album['poster'] ?>" alt="<?php echo $album['title'] . 'di' . $album['author']?>">
+                        </div>
+                        <h3 class="title-album"><?php echo $album['title'] ?></h3>
+                        <div class="info-album">
+                            <div class="artist"><?php echo $album['author'] ?></div>
+                            <div class="year"><?php echo $album['year'] ?></div>
+                        </div>
+
+                    </div>
+                </div>
+                <?php } ?>
+
+
+            </div>
+        </div>
+    </main>
+
+    <?php
+
+        foreach ($albums as $album) {
+            echo $album['title'];
+        }
+    ?>
     
 </body>
 </html>
